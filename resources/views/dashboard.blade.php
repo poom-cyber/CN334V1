2721223d471111708d2212bcf93faaa9e92c0df4
poom-cyber <poompim083@gmail.com>
2022-02-21 23:50:59 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,8 @@
             <a href="/allTank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">See All Task</a>
             <a href="/task" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add New Task</a>
             <a href="/editTank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit Task</a>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=YourPageLink.com&display=popup"> share this </a>
+
             </div>
             <br>
             
