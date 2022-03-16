3a84e8b548d9d70ebcde2335422bc7ff807451c2
poom-cyber <poompim083@gmail.com>
2022-03-16 13:27:33 +0700

    Commit route test

---
+++ tests/Unit/RegisterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
+use Laravel\Jetstream\Jetstream;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+
+    public function test_registration_screen_can_be_rendered()
+    {
+        if (! Features::enabled(Features::registration())) {
+            return $this->markTestSkipped('Registration support is not enabled.');
+        }
+
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+
+
+}
