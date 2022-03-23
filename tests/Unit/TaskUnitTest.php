f63bd70e74b28d7ec4fb247a8c0a6a53e91b1005
KCcie <kthapnp@gmail.com>
2022-03-23 11:24:14 +0700

    plan unit

---
+++ tests/Unit/TaskUnitTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskUnitTest extends TestCase
+{
+    // test shema test description pass
+    public function test_schema_description()
+    {
+
+    }
+
+    // test shema test user_id pass
+    public function test_schema_user_id()
+    {
+
+    }
+
+    // description
+    // test shema ทดสอบ รองรับภาษาไทย
+    // test shema ทดสอบ รองรับภาษาอังกฤษไหม
+    // test shema ทดสอบ ใส่ตัวเลขได้ไหม
+    // test shema ทดสอบ ใส่คำสั่ง SQL ได้ไหม
+    // test shema ทดสอบ ความยาวตัวอักษร
+    
+    // id
+    // test shema ทดสอบ เป็น null ได้มั้ย
+    // test shema ทดสอบ เป็น integer ได้ไหม
+}
